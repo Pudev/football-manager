f3918fd900b84ed0bfc52825db6f3ca2af42861c
Dimitar Pudev <dpudev@gmail.com>
2019-01-14 23:54:31 +0200

    Add invited players counter
    close #29

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,9 +8,11 @@ class Home extends React.Component<any, any> {
         super(props);
 
         var homeTeamPlayingTotal = this.props.players.filter((x: IPlayer) => x.team === constants.PlayerTeam.Home && x.status === constants.PlayerStatus.WillPlay);
+        var homeTeamInvitedTotal = this.props.players.filter((x: IPlayer) => x.team === constants.PlayerTeam.Home && x.status === constants.PlayerStatus.Invited);
         var awayTeamPlayingTotal = this.props.players.filter((x: IPlayer) => x.team === constants.PlayerTeam.Away && x.status === constants.PlayerStatus.WillPlay);
+        var awayTeamInvitedTotal = this.props.players.filter((x: IPlayer) => x.team === constants.PlayerTeam.Away && x.status === constants.PlayerStatus.Invited);
 
-        this.state = { homeTeamPlayingTotal, awayTeamPlayingTotal };
+        this.state = { homeTeamPlayingTotal, homeTeamInvitedTotal, awayTeamPlayingTotal, awayTeamInvitedTotal };
     }
 
     render() {
@@ -28,6 +30,7 @@ class Home extends React.Component<any, any> {
                         editPlayer={this.props.editPlayer}
                         deletePlayer={this.props.deletePlayer} />
                     <div className="col-12">Number of players that will play: <span className="font-bold">{this.state.homeTeamPlayingTotal.length}</span></div>
+                    <div className="col-12">Number of players that are invited: <span className="font-bold">{this.state.homeTeamInvitedTotal.length}</span></div>
                 </div>
                 <div className="col-sm-6">
                     <Team
@@ -41,6 +44,7 @@ class Home extends React.Component<any, any> {
                         editPlayer={this.props.editPlayer}
                         deletePlayer={this.props.deletePlayer} />
                     <div className="col-12">Number of players that will play: <span className="font-bold">{this.state.awayTeamPlayingTotal.length}</span></div>
+                    <div className="col-12">Number of players that are invited: <span className="font-bold">{this.state.awayTeamInvitedTotal.length}</span></div>
                 </div>
             </div>
         );
