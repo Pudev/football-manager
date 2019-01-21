381c60eb0b3b8584f79fea6ce8a90162f27a50bc
Dimitar Pudev <dpudev@gmail.com>
2019-01-21 22:49:18 +0200

    Update Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -12,14 +12,14 @@ class Main extends React.Component<any, any> {
         return (
             <main>
                 <Switch>
-                    <Route exact path='/' basename={process.env.PUBLIC_URL} component={() => <Home
+                    <Route exact path={`${process.env.PUBLIC_URL}/`} component={() => <Home
                         addPlayer={this.props.addPlayer}
                         editPlayer={this.props.editPlayer}
                         deletePlayer={this.props.deletePlayer}
                         players={this.props.players}
                         playerStatusFilter={this.props.playerStatusFilter} />}
                     />
-                    <Route path='/about' basename={process.env.PUBLIC_URL} component={About} />
+                    <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
                 </Switch>
             </main>
         );
